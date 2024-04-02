57d85e4e989a016192d63aeffb1b76134856f2ba
Alessandra Schiavi Paz <alessandra.schiavipa@ucalgary.ca>
2024-04-02 14:15:15 -0600

    removed an import in loginpage that is not needed.

--- Assignment1/my-ecommerce-app/src/component/LoginPage.js
+++ Assignment1/my-ecommerce-app/src/component/LoginPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import LoginForm from './LoginForm';
-import SignupForm from './SignupForm';
+
 
 function LoginPage(){
 

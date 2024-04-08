7af5787f27974355236c91e260024e1ff10e0cc3
Nikita Williams <nikita.williams@ucalgary.ca>
2024-04-07 18:19:06 -0600

    pushing my code

--- Assignment1/my-ecommerce-app/src/component/LoginForm.js
+++ Assignment1/my-ecommerce-app/src/component/LoginForm.js
@@ -1,25 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function LoginForm(){
     const validateLogin = async () => {
         try {
-            const response = await axios.post('http://localhost:5000/login', {
+            /*const response = await axios.post('http://localhost:5000/login', {
                 username: document.getElementById('username').value,
                 password: document.getElementById('pword').value
             });
             console.log(response.data); // Handle successful login response
-            // Redirect or update UI accordingly
+            // Redirect or update UI accordingly*/
+            console.log('testing');
         } catch (error) {
-            console.error('Error:', error.response.data); // Handle error response
+            /*console.error('Error:', error.response.data); // Handle error response
             // Update UI to display error message
-            alert('User does not exist');
+            alert('User does not exist');*/
         }
     }
 
+    const [formData, setFormData] = useState({
+        username: '',
+        pword: '',
+    });
+
+    const [formErrors, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({...formData, [name]: value });
+        console.log(formData);
+    };
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+        console.log("Form Data:", formData)
+        setFormErrors(validate(formData));
+        setIsSubmit(true);
+      };
+
+      useEffect(() => {
+        console.log(formErrors);
+        if(Object.keys(formErrors).length === 0 && isSubmit) {
+            console.log(formData);
+        }
+      }, [formErrors]);
+
+    const validate = (values) => {
+        const errors = {}
+        console.log('Inside validate');
+        if (!values.username) {
+            errors.username = "Username is required!";
+            console.log(errors.username);
+        }
+
+        if (!values.pword) {
+            errors.pword = "Password is required!";
+            console.log(errors.pword);
+        }
+        console.log(errors);
+
+        return errors;
+    };
+
     return(
         <>
-        <form>
+        <form onSubmit={onSubmitHandler}>
         <br />
         <h2>Login</h2>
         <div>
@@ -30,10 +76,13 @@ function LoginForm(){
                 id="username"
                 name="username"
                 placeholder="Enter your username"
-                required=""
+                //required=""
+                value={formData.username}
+                onChange={handleChange}
                 autoComplete="username"
             />
             </div>
+            <p style={{ color: 'red' }}>{formErrors.username}</p>
             <div>
             <label htmlFor="pword">Password:</label>
             <input
@@ -41,12 +90,15 @@ function LoginForm(){
                 id="pword"
                 name="pword"
                 placeholder="Enter your password"
-                required=""
+                //required=""
+                value={formData.pword}
+                onChange={handleChange}
                 autoComplete="current-password"
             />
             </div>
+            <p style={{ color: 'red' }}>{formErrors.pword}</p>
             <div>
-            <button onclick={validateLogin()} type="button">
+            <button onClick={validateLogin} /*type="button"*/>
                 Login
             </button>
             </div>

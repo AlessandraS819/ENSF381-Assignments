374764ad8a0fcff5b2f1f43d0f77c7dc1b22c94f
Alessandra Schiavi Paz <alessandra.schiavipa@ucalgary.ca>
2024-04-08 13:57:30 -0600

    Fixed the navigation and login form. Now I need to figure out how to keep the status even if the page is reloaded.

--- Assignment1/my-ecommerce-app/src/App.js
+++ Assignment1/my-ecommerce-app/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import './App.css';
 import HomePage from './component/Homepage.js';
 import ProductPage from './component/Productpage.js';
@@ -8,14 +8,15 @@ import LoginPage from './component/LoginPage.js';
 import SignupForm from './component/SignupForm.js';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <>
       <BrowserRouter>
         <Routes>
         <Route path="/" element={<HomePage />} ></Route>
-        <Route path="/products" element={<ProductPage />}>
+        <Route path="/products" element={isLoggedIn ? (<ProductPage />): (<Navigate to="/login" replace={true}/>)}>
         </Route>
-          <Route path= "/login" element = {<LoginPage/>}/>
+          <Route path= "/login" element = {<LoginPage setIsLoggedIn={setIsLoggedIn}/>}/>
           <Route path= "/signup" element = {<SignupForm/>}/>
         </Routes>
         
